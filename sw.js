c3c2771807fa74d036f9836dd5113f6093f685a1
fmcine <fmcine@gmail.com>
2026-06-20 09:42:37 +0900

    feat: update SW cache name to llama-cache-v1, add cache cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'memo-cache-v1';
+const CACHE_NAME = 'llama-cache-v1';
 const ASSETS = [
   'index.html',
   'manifest.json',
@@ -9,6 +9,16 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
     })
   );
 });
